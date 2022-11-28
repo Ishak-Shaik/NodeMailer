71f612f28dbdde26e6045863a0025670c3d56dcd
Ishak-Shaik <116098107+Ishak-Shaik@users.noreply.github.com>
2022-11-29 01:04:09 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+const nodemailer=require('nodemailer');
+
+const  transporter=nodemailer.createTransport({
+    service:'hotmail',
+    auth:{
+        user:'nodeproject123@outlook.com',
+        pass:'nodeMailer@123'
+    }
+});
+
+const options={
+    from:'nodeproject123@outlook.com',
+    to:'ishaksk199@gmail.com',
+    subject:'testing the nodemailer',
+    text:'Hello...How are you??'
+}
+
+transporter.sendMail(options,(err,info)=>{
+    if(err){
+        console.log('error occured');
+        return;
+    }
+    console.log(info);
+})
